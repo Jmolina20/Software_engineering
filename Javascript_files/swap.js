b39434a578127ece5af412b16ad95b4d2ba228f7
Jmolina20 <julianmolina2011@gmail.com>
2021-03-29 11:02:20 -0400

    Brief update to rename files and folders

---
+++ Javascript_files/swap.js
@@ -0,0 +1,8 @@
+var num1=2;
+var num2=1;
+var temp;
+console.log("The original set up is where num1 = " + num1 + " and num2 = " + num2);
+temp=num1;
+num1=num2;
+num2=num1;
+console.log("The new set up is where num1 = " + num1 + " and num2 = " + num2);
--- Javascript_files/test.js
+++
@@ -1,5 +0,0 @@
-var num1=2;
-var num2=1;
-var test="Hello world";
-console.log(num1);
-console.log(test);
