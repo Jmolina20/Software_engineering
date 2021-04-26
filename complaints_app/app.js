93566ac857997da165b8c866e375cca57714a1ab
Jmolina20 <julianmolina2011@gmail.com>
2021-04-25 23:25:06 -0400

    some brief changes

---
+++ complaints_app/app.js
@@ -0,0 +1,23 @@
+
+
+function queens_data (){
+    fetch("https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=QUEENS");}
+    
+function bronx_data(){
+    fetch("https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=BRONX");
+    }
+function brooklyn_data (){
+    fetch("https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=BROOKLYN");
+}
+
+function staten_island_data () {
+    fetch("https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=STATENISLAND");
+}
+
+function manhattan_data () {
+    fetch("https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=MANHATTAN");
+}
+
+function clear_data() {
+    
+}
