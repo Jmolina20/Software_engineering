18b17b01100d2c42e439ade513bb3e5ea5d8a0bf
Jmolina20 <julianmolina2011@gmail.com>
2021-05-11 18:10:20 -0400

    changes including tardis assignment

---
+++ react/tardis/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+//render is a state property
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
